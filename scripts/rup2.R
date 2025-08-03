538c552e3505e94b9e42d7e539b77fb2911886f3
Oliver Rupp <oliver.rupp@computational.bio.uni-giessen.de>
2025-08-03 08:07:08 +0200

    cleanup

--- scripts/rup2.R
+++ scripts/rup2.R
@@ -1,16 +1,26 @@
-library(tidyr)
-library(dplyr)
-library(tidyverse)
-library(ggplot2)
-library(reshape2)
-library(pheatmap)
-library(ComplexHeatmap)
-library(rjson)
-library(tximeta)
-library(BiocFileCache)
-library(DESeq2)
-library(gtools)
-library(getopt)
+suppressMessages(library(tidyr))
+suppressMessages(library(dplyr))
+suppressMessages(library(tidyverse))
+suppressMessages(library(ggplot2))
+suppressMessages(library(reshape2))
+suppressMessages(library(pheatmap))
+suppressMessages(library(ComplexHeatmap))
+suppressMessages(library(rjson))
+suppressMessages(library(tximeta))
+suppressMessages(library(AnnotationDbi))
+suppressMessages(library(BiocFileCache))
+suppressMessages(library(DESeq2))
+suppressMessages(library(gtools))
+suppressMessages(library(getopt))
+suppressMessages(library(xlsx))
+suppressMessages(library(moments))
+suppressMessages(library(PCAtools))
+
+
+
+
+
+##################### INIT #####################
 
 min_assigned_read_count <- 10e6
 min_sample_correlation <- 0.9
@@ -19,9 +29,6 @@ spec = matrix(c(
   'datafolder' , 'd', 1, "character",
   'reads', 'r', 1, 'integer',
   'correlation', 'c', 1, "double",
-#  'make', 'm', 0, "",
-#  'threads', 't', 1, "integer",
-#  'jobs', 'j', 1, "integer",
   'help', 'h', 0, ""
 ), byrow=TRUE, ncol=4)
 opt = getopt(spec)
@@ -30,15 +37,28 @@ if(!is.null(opt$datafolder)) setwd(opt$datafolder)
 if(!is.null(opt$reads)) min_assigned_read_count <- opt$reads
 if(!is.null(opt$correlation)) min_sample_correlation <- opt$correlation
 
-### setwd("~/Projects/rnaseq_ranomics/CR")
-
 outfile="report.pdf"
 
 if(exists("snakemake")) {
+
+  message(snakemake@output[["report"]])
+
   setwd(dirname(snakemake@output[["report"]]))
   min_assigned_read_count = snakemake@params[["reads"]]
   outfile = basename(snakemake@output[["report"]])
 }
+
+pdf(outfile, w=18, h=12)
+
+theme_set(theme_bw())
+
+##########################################
+
+
+
+
+
+##################### SAMPLES #####################
 
 sample_info <- read.table("reference/samples.tsv", header=F, sep="\t", row.names = 2)
 colnames(sample_info) <- "condition"
@@ -47,49 +67,19 @@ sname = mixedsort(unique(sample_info$condition))
 sample_colors = rainbow(length(sname))
 names(sample_colors) = sname
 
-
-
-
-
-
 sample_info$files = paste0("results/salmon/", rownames(sample_info), "/quant.sf")
 sample_info$names = rownames(sample_info)
 
-bfcDir <- file.path(getwd(),"results/index/BFC")
-indexDir <- file.path(getwd(),"results/index/salmon")
-fastaPath <- file.path(getwd(),"reference/genome.fa")
-gtfPath <- file.path(getwd(),"reference/annotation.gtf")
-
-#bfcloc <- getTximetaBFC()
-#message(bfcloc)
-#unlink(paste0(bfcloc,"/*"))
-
-setTximetaBFC(bfcDir)
-makeLinkedTxome(indexDir=indexDir,
-                source="denovo",
-                organism=getwd(),
-                release="1",
-                genome=getwd(),
-                fasta=fastaPath,
-                gtf=gtfPath)
-
-se <- tximeta(sample_info)
-gse <- summarizeToGene(se)
-
-TPM = assay(gse, "abundance")
-gene_count_matrix = assay(gse, "counts")
-dds <- DESeqDataSet(gse, design = ~condition)
-vsd <- vst(dds, blind = FALSE)
-cor_data = assay(vsd)
-#cor_data = log2(TPM+1)
-
-
-
-
-
 samples <- row.names(sample_info)
 
-deg_res <- data.frame(sample=character(), quantile=character(), pct=integer())
+##########################################
+
+
+
+
+
+##################### READ COUNTS #####################
+
 results <- data.frame(sample=character(), status=character(), reference=character(), reads=integer())
 
 for(sample in samples) {
@@ -122,13 +112,136 @@ for(sample in samples) {
   if(file.exists(jfile)) {
     jdat <- fromJSON(file = jfile)
 
-    stats <- data.frame(status=c("Assigned"),
+    stats <- data.frame(status=c("rRNA"),
                         reads=c(jdat$num_mapped))
 
     stats$sample <- sample
-    stats$reference <- "rRNA"
+    stats$reference <- "Gene"
     results <- rbind(results, stats)
+    results[results$status == "Unmapped" & results$sample == sample,]$reads = 
+            results[results$status == "Unmapped" & results$sample == sample,]$reads  - jdat$num_mapped
   }
+}
+
+results$status = factor(results$status, levels=rev(c("Assigned", "rRNA", "NoFeature", "Unmapped",
+                                                     "Nreads", "TooShort", "TooLong", "LowQuality")))
+reads_plot <- ggplot(results, aes(x=sample, y=reads, fill=status)) + 
+  #geom_hline(yintercept=c(30e6, 50e6)) + 
+  geom_bar(stat="identity") +
+  theme(text = element_text(size = 18)) + ggtitle("Read count analysis") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 0)) #+ facet_wrap(~reference, ncol=1)
+
+print(reads_plot)
+
+##########################################
+
+
+
+
+
+##################### COLLECT COUNTS #####################
+
+bfcDir <- file.path(getwd(),"results/index/BFC")
+indexDir <- file.path(getwd(),"results/index/salmon")
+fastaPath <- file.path(getwd(),"reference/genome.fa")
+gtfPath <- file.path(getwd(),"reference/annotation.gtf")
+
+if(!dir.exists(bfcDir)) { dir.create(bfcDir) }
+
+setTximetaBFC(bfcDir)
+makeLinkedTxome(indexDir=indexDir,
+                source="denovo",
+                organism=getwd(),
+                release="1",
+                genome=getwd(),
+                fasta=fastaPath,
+                gtf=gtfPath)
+
+se <- tximeta(sample_info)
+gse <- summarizeToGene(se)
+
+gene_count_matrix <- assay(gse, "counts")
+dds <- DESeqDataSet(gse, design = ~condition)
+vsd <- vst(dds, blind = FALSE)
+cor_data <- assay(vsd)
+
+sample_cor <- cor(cor_data, method='pearson', use='pairwise.complete.obs')
+sample_dist <- as.matrix(dist(t(cor_data)))
+
+##########################################
+
+
+
+
+
+##################### READS/GENE COUNTS #####################
+
+df = data.frame("G0"=colSums(gene_count_matrix == 0),
+                "G1"=colSums(gene_count_matrix > 0 & gene_count_matrix <= 10),
+                "G10"=colSums(gene_count_matrix > 10 & gene_count_matrix < 25),
+                "G100"=colSums(gene_count_matrix >= 25))
+
+df$Replicate = rownames(df)
+df$Sample = sample_info[df$Replicate,]$condition
+
+dfm = melt(df, id.vars = c("Replicate", "Sample"))
+for (sample_name in unique(sample_info$condition)) {
+  message(sample_name)
+  if(sum(sample_info$condition == sample_name) > 1) {
+  	max_counts <- apply(gene_count_matrix[,sample_info[sample_info$condition == sample_name,]$names], 1, max, na.rm=TRUE)
+
+  	dfm <- rbind(dfm, list(Replicate = sample_name, Sample = sample_name, variable="G0", value=sum(max_counts == 0)))
+  	dfm <- rbind(dfm, list(Replicate = sample_name, Sample = sample_name, variable="G1", value=sum(max_counts > 0 & max_counts <= 10)))
+  	dfm <- rbind(dfm, list(Replicate = sample_name, Sample = sample_name, variable="G10", value=sum(max_counts > 10 & max_counts < 25)))
+  	dfm <- rbind(dfm, list(Replicate = sample_name, Sample = sample_name, variable="G100", value=sum(max_counts >= 25)))
+  }
+}
+
+dfm$variable = as.character(dfm$variable)
+
+dfm[dfm$variable == "G0",]$variable = "No reads"
+dfm[dfm$variable == "G1",]$variable = "Low read count"
+dfm[dfm$variable == "G10",]$variable = "Medium read count"
+dfm[dfm$variable == "G100",]$variable = "Good read count"
+
+dfm$variable = factor(dfm$variable, levels=c("No reads", "Low read count", "Medium read count", "Good read count"))
+
+dfm$Replicate = as.character(dfm$Replicate)
+for(s in unique(dfm$Sample)) {
+  dfm[dfm$Sample == s,]$Replicate = gsub(paste0(s, "_"), "", dfm[dfm$Sample == s,]$Replicate)
+  dfm[dfm$Sample == s,]$Replicate = gsub(paste0(s, "-"), "", dfm[dfm$Sample == s,]$Replicate)
+  dfm[dfm$Sample == s,]$Replicate = gsub(s, "S", dfm[dfm$Sample == s,]$Replicate)
+}
+
+dfm$Replicate = factor(dfm$Replicate, levels=c(mixedsort(unique(dfm$Replicate)), mixedsort(unique(dfm$Sample))))
+
+gcp_colors = c("#a0a0a0", "#F8766D", "#C49A00", "#00BA38")
+names(gcp_colors) = levels(dfm$variable)
+
+gene_coverage_plot <- ggplot(dfm, aes(x=Replicate, y=value, fill=variable)) +
+  geom_bar(stat="identity") +
+  ylab("Number of Genes") + xlab("Samples") +
+  ggtitle("Number of Reads per Gene") +
+  guides(fill=guide_legend(title="Number of assigned reads")) +
+  theme(text = element_text(size = 18)) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 0)) +
+  facet_wrap(~Sample, scale="free_x") +
+  scale_fill_manual(values = gcp_colors)
+
+print(gene_coverage_plot)
+
+##########################################
+
+
+
+
+
+##################### GENE BODY COVERAGE #####################
+
+deg_res <- data.frame(sample=character(), quantile=character(), pct=integer())
+
+for(sample in samples) {
+  message(sample)
 
   jfile <- paste0("results/salmon_quantiles/", sample, "/quant.sf")
 
@@ -157,73 +270,34 @@ deg_tmp = pivot_wider(deg_res, names_from = quantile, values_from = pct)
 deg_matrix = as.matrix(column_to_rownames(deg_tmp, var="sample"))
 deg_matrix = deg_matrix[,paste0("q", 1:10)]
 
+ComplexHeatmap::pheatmap(deg_matrix, cluster_rows = T, cluster_cols = F,
+         treeheight_row=0, fontsize = 14, border = F,
+         heatmap_legend_param=list(title="coverage (%)"), main="RNA degradation (gene body coverage)")
 
 
-results$status = factor(results$status, levels=rev(c("Assigned", "NoFeature", "Unmapped",
-                                                     "Nreads", "TooShort", "TooLong", "LowQuality")))
-reads_plot <- ggplot(results, aes(x=sample, y=reads, fill=status)) + geom_bar(stat="identity") +
-  theme(text = element_text(size = 18)) +
-  theme(axis.text.x = element_text(angle = 90, hjust = 0)) + facet_wrap(~reference, ncol=1)
-
-
+##########################################
 
 
 
-df = data.frame("G0"=colSums(gene_count_matrix == 0),
-                "G1"=colSums(gene_count_matrix >= 1 & gene_count_matrix < 10),
-                "G10"=colSums(gene_count_matrix >= 10 & gene_count_matrix < 100),
-                "G100"=colSums(gene_count_matrix >= 100 & gene_count_matrix < 1000),
-                "G1000"=colSums(gene_count_matrix >= 1000))
-
-df$Replicate = rownames(df)
-df$Sample = sample_info[df$Replicate,]$condition
-
-dfm = melt(df, id.vars = c("Replicate", "Sample"))
-for (sample_name in unique(sample_info$condition)) {
-  message(sample_name)
-  if(sum(sample_info$condition == sample_name) > 1) {
-  	max_counts <- apply(gene_count_matrix[,sample_info[sample_info$condition == sample_name,]$names], 1, max, na.rm=TRUE)
-
-  	dfm <- rbind(dfm, list(Replicate = sample_name, Sample = sample_name, variable="G0", value=sum(max_counts == 0)))
-  	dfm <- rbind(dfm, list(Replicate = sample_name, Sample = sample_name, variable="G1", value=sum(max_counts >= 1 & max_counts < 10)))
-  	dfm <- rbind(dfm, list(Replicate = sample_name, Sample = sample_name, variable="G10", value=sum(max_counts >= 10 & max_counts < 100)))
-  	dfm <- rbind(dfm, list(Replicate = sample_name, Sample = sample_name, variable="G100", value=sum(max_counts >= 100 & max_counts < 1000)))
-  	dfm <- rbind(dfm, list(Replicate = sample_name, Sample = sample_name, variable="G1000", value=sum(max_counts >= 1000)))
-  }
-}
-
-dfm$variable = as.character(dfm$variable)
-
-dfm[dfm$variable == "G0",]$variable = "No reads"
-dfm[dfm$variable == "G1",]$variable = "1 to 10 reads"
-dfm[dfm$variable == "G10",]$variable = "10 to 100 reads"
-dfm[dfm$variable == "G100",]$variable = "100 to 1000 reads"
-dfm[dfm$variable == "G1000",]$variable = "more than 1000 reads"
-
-dfm$variable = factor(dfm$variable, levels=c("No reads", "1 to 10 reads", "10 to 100 reads", "100 to 1000 reads", "more than 1000 reads"))
-
-dfm$Replicate = as.character(dfm$Replicate)
-for(s in unique(dfm$Sample)) {
-  dfm[dfm$Sample == s,]$Replicate = gsub(paste0(s, "_"), "", dfm[dfm$Sample == s,]$Replicate)
-  dfm[dfm$Sample == s,]$Replicate = gsub(paste0(s, "-"), "", dfm[dfm$Sample == s,]$Replicate)
-  dfm[dfm$Sample == s,]$Replicate = gsub(s, "S", dfm[dfm$Sample == s,]$Replicate)
-}
-
-dfm$Replicate = factor(dfm$Replicate, levels=c(mixedsort(unique(dfm$Replicate)), mixedsort(unique(dfm$Sample))))
-
-gene_coverage_plot <- ggplot(dfm, aes(x=Replicate, y=value, fill=variable)) +
-  geom_bar(stat="identity") +
-  ylab("Number of Genes") + xlab("Samples") +
-  ggtitle("Number of Reads per Gene") +
-  guides(fill=guide_legend(title="Number of assigned reads")) +
-  theme(text = element_text(size = 10)) +
-  theme(axis.text.x = element_text(angle = 90, hjust = 0)) +
-  facet_wrap(~Sample, scale="free_x")
-
-sample_cor <- cor(cor_data, method='pearson', use='pairwise.complete.obs')
-sample_dist <- as.matrix(dist(t(cor_data)))
 
 
+##################### SAMPLE CORRELATION #####################
+
+a = data.frame(samples = sample_info[colnames(cor_data),1])
+row.names(a) = colnames(cor_data)
+
+pheatmap::pheatmap(sample_cor, fontsize = 14, annotation_col=a, main="Sample Correlation Heatmap of VST counts")
+
+PCA = pca(cor_data, metadata = colData(se))
+biplot(PCA, x = "PC2", y = "PC1", colby="condition", title="PCA of VST counts")
+
+##########################################
+
+
+
+
+
+##################### FILTER SAMPLES #####################
 
 remove_by_reads <- results[results$status == "Assigned" & results$reference == "Gene" & results$reads <= min_assigned_read_count,]$sample
 remove_by_correlation <- c()
@@ -234,23 +308,24 @@ for(sample_name in unique(sample_info$condition)) {
   message(sample_name)
 
   if(sum(sample_info$condition == sample_name) > 2) {
-  sample_replicates <- sample_info[sample_info$condition == sample_name,]$names
-  sample_replicates <- sample_replicates[! sample_replicates %in% remove_by_reads]
+    sample_replicates <- sample_info[sample_info$condition == sample_name,]$names
+    sample_replicates <- sample_replicates[! sample_replicates %in% remove_by_reads]
 
-  if(length(sample_replicates) > 1) {
-    rep_num <- sum(sample_info$condition == sample_name)
-    low_cor <- rowSums(sample_cor_filtered[sample_replicates, sample_replicates] <= min_sample_correlation)
-    if(sum(low_cor/rep_num > 0.67)) {
-      low_ids = names(low_cor[low_cor/rep_num > 0.67])
-      remove_by_correlation <- c(remove_by_correlation, low_ids)
+    if(length(sample_replicates) > 1) {
+      rep_num <- sum(sample_info$condition == sample_name)
+      low_cor <- rowSums(sample_cor_filtered[sample_replicates, sample_replicates] <= min_sample_correlation)
+      if(sum(low_cor/rep_num > 0.67)) {
+        low_ids = names(low_cor[low_cor/rep_num > 0.67])
+        remove_by_correlation <- c(remove_by_correlation, low_ids)
+      }
     }
   }
-}
 }
 
 low_q_samples <- c(remove_by_reads, remove_by_correlation)
 
-plot_rem <- F
+message(low_q_samples)
+
 if(length(low_q_samples) > 0) {
   lowq <- data.frame(sample = low_q_samples, reason = "low read number")
   if(length(remove_by_correlation) > 0) {
@@ -261,64 +336,99 @@ if(length(low_q_samples) > 0) {
     theme(text = element_text(size = 18)) +
       ylab("Replicates") + xlab(NULL) +
       ggtitle("Low Quality Samples")
-  plot_rem <- T
+
+  print(removed_plot)
 }
 
-
-message(low_q_samples)
-
-if(length(low_q_samples) < ncol(cor_data)) {
-cor_data_filtered = cor_data[,!colnames(cor_data) %in% low_q_samples]
-
-sample_cor_filtered <- cor(cor_data_filtered, method='pearson', use='pairwise.complete.obs')
-sample_dist_filtered <- as.matrix(dist(t(cor_data_filtered)))
-}
-
-library(PCAtools)
+##########################################
 
 
-pdf(outfile, w=18, h=12)
 
-theme_set(theme_bw())
 
-print(reads_plot)
-print(gene_coverage_plot)
 
-pheatmap(deg_matrix, cluster_rows = T, cluster_cols = F,
-         treeheight_row=0, fontsize = 8, border = F,
-         heatmap_legend_param=list(title="coverage (%)"))
+##################### FILTERED SAMPLES PLOT #####################
 
-detach("package:ComplexHeatmap", unload=TRUE)
+if(length(low_q_samples) > 0 && length(low_q_samples) < ncol(cor_data)) {
+  message("FILTERED:")
+  message(length(low_q_samples))
+  message(ncol(cor_data))
+  cor_data_filtered = cor_data[,!colnames(cor_data) %in% low_q_samples]
 
-a = data.frame(samples = sample_info[colnames(cor_data),1])
-row.names(a) = colnames(cor_data)
+  sample_cor_filtered <- cor(cor_data_filtered, method='pearson', use='pairwise.complete.obs')
+  sample_dist_filtered <- as.matrix(dist(t(cor_data_filtered)))
 
-pheatmap(sample_cor, fontsize = 10, annotation_col=a, main="Sample Correlation Heatmap")
-pheatmap(sample_dist, fontsize = 10, annotation_col=a, main="Sample Distance Heatmap")
-
-PCA = pca(cor_data, metadata = colData(se))
-biplot(PCA, x = "PC2", y = "PC1", colby="condition")
-
-if(plot_rem) { print(removed_plot) }
-
-if(!is.null(sample_cor_filtered)) {
-  pheatmap(sample_cor_filtered, fontsize = 10,
-           annotation_col=a, main="Sample Correlation Heatmap")
-
-  pheatmap(sample_dist_filtered, fontsize = 10, annotation_col=a, main="Sample Distance Heatmap")
+   pheatmap::pheatmap(sample_cor_filtered, fontsize = 10,
+           annotation_col=a, main="Filtered Sample Correlation Heatmap")
 
   PCA = pca(cor_data_filtered, metadata = colData(se)[! rownames(colData(se)) %in% low_q_samples,])
-  bip = biplot(PCA, x = "PC2", y = "PC1", colby="condition")
-  pair = pairsplot(PCA, colby="condition")
-  scree = screeplot(PCA, axisLabSize = 18, titleLabSize = 22)
-
-  print(bip)
-  print(pair)
-  print(scree)
+  biplot(PCA, x = "PC2", y = "PC1", colby="condition", title="PCA of VST counts (filtered samples)")
 }
+
+##########################################
+
+
+
+
+
+##################### (FILTERED) PCA PLOTS #####################
+
+pair = pairsplot(PCA, colby="condition")
+scree = screeplot(PCA, axisLabSize = 18, titleLabSize = 22)
+
+print(pair)
+print(scree)
+
+##########################################
+
+
+
+
+
+##################### WRITE PDF #####################
 
 dev.off()
 
+##########################################
 
 
 
+
+
+##################### WRITE DATA #####################
+
+xsheet1 = pivot_wider(results[results$reference == "Gene",], id_cols="sample", names_from="status", values_from="reads")
+
+xsheet1$trimmed = xsheet1$LowQuality+xsheet1$Nreads+xsheet1$TooShort+xsheet1$TooLong
+
+xsheet1 = xsheet1 %>% dplyr::select(-one_of("LowQuality", "Nreads", "TooShort", "TooLong"))
+
+rrnar = results[results$reference == "rRNA",]$reads
+
+names(rrnar) = results[results$reference == "rRNA",]$sample
+
+xsheet1 = cbind(xsheet1, rRNA=rrnar[xsheet1$sample])
+
+skewn=skewness(t(deg_matrix))
+
+xsheet1 = cbind(xsheet1, skewness=skewn[xsheet1$sample])
+
+xsheet2 = df
+
+xsheet3 = sample_cor
+
+xsheet4 = assay(dds, "counts")
+
+xsheet5 = assay(dds, "abundance")
+
+write.table(xsheet1, paste0(outfile, ".report.tsv"), sep="\t", quote=F, col.names=TRUE, row.names=FALSE, append=FALSE)
+write.xlsx2(xsheet1, paste0(outfile, ".report.xlsx"), sheetName="sequencing results", col.names=TRUE, row.names=FALSE, append=FALSE)
+
+write.xlsx2(xsheet2, paste0(outfile, ".report.xlsx"), sheetName="reads per gene", col.names=TRUE, row.names=FALSE, append=TRUE)
+
+write.xlsx2(xsheet3, paste0(outfile, ".report.xlsx"), sheetName="sample correlation", col.names=TRUE, row.names=TRUE, append=TRUE)
+
+write.table(xsheet4, paste0(outfile, ".raw.counts.tsv"), sep="\t", quote=F, col.names=TRUE, row.names=TRUE, append=TRUE)
+
+write.table(xsheet5, paste0(outfile, ".TMP.normalized.tsv"), sep="\t", quote=F, col.names=TRUE, row.names=TRUE, append=FALSE)
+
+##########################################
